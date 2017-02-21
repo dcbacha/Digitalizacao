eccc504c80a06cb0b14015ed2c49ad7a8bb549bd
dcbacha <dcbacha@gmail.com>
2017-02-21 11:29:14 -0400

    Salvando relatorio em pdf multilas linhas

--- SalvarParaPDF.php
+++ SalvarParaPDF.php
@@ -4,12 +4,53 @@ require_once("config.php");
 
 header("Content-Type: application/json; charset=utf-8");
 
-
-
+ 
 
 ob_start();
-?>
 
+          $i=0;
+        // $tamanho =0;
+         if(isset($_POST['dados'])){
+            $_SESSION['sessao'] = 0; // acesso por ajax
+
+            $dados = json_decode($_POST['dados'], true);
+            
+
+            $_SESSION['tamanho'] = count($dados);
+
+            $itenstabela = json_decode($dados[0], true);
+          //  $_SESSION['teste'] = $dados['orden_de_pago'][1];
+             $_SESSION['teste'] = $itenstabela[0][0];
+
+            ?>
+    
+          <?php
+
+
+        foreach ($dados as $result){
+        //  for($i=0 ; $i < $_SESSION['tamanho'] ; $i++ ){
+        /*  $_SESSION['fecha_de_emision'] = $dados['fecha_de_emision'];
+          $_SESSION['orden_de_pago'] = $dados['orden_de_pago'];
+          $_SESSION['estado'] = $dados['estado'];
+          $_SESSION['credor'] = $dados['credor'];
+          $_SESSION['empresa'] = $dados['empresa'];
+          $_SESSION['cheque'] = $dados['cheque'];
+          $_SESSION['ruc'] = $dados['ruc'];*/
+
+          $_SESSION['fecha_de_emision'.$i] = $result['fecha_de_emision'];
+          $_SESSION['orden_de_pago'.$i] = $result['orden_de_pago'];
+          $_SESSION['estado'.$i] = $result['estado'];
+          $_SESSION['credor'.$i] = $result['credor'];
+          $_SESSION['empresa'.$i] = $result['empresa'];
+          $_SESSION['cheque'.$i] = $result['cheque'];
+          $_SESSION['ruc'.$i] = $result['ruc'];
+          $i++;
+        }
+      }else{
+        $_SESSION['sessao'] = 1; //acesso para download
+      }
+
+?>
 
 
 
@@ -38,47 +79,32 @@ ob_start();
         </tr>
       </thead>
       <tbody>    
-       
-        <tr>
-         <?php
-
-         if(isset($_POST["dados"])){
-            $dados = json_decode($_POST["dados"], true);
-
-          $_SESSION['fecha_de_emision'] = $dados['fecha_de_emision'];
-          $_SESSION['orden_de_pago'] = $dados['orden_de_pago'];
-          $_SESSION['estado'] = $dados['estado'];
-          $_SESSION['credor'] = $dados['credor'];
-          $_SESSION['empresa'] = $dados['empresa'];
-          $_SESSION['cheque'] = $dados['cheque'];
-          $_SESSION['ruc'] = $dados['ruc'];
-
-          
-          }
-         
-
-      /*    $fecha_de_emision = $result['fecha_de_emision'];
+           <?php
+        /*    $fecha_de_emision = $result['fecha_de_emision'];
           $orden_de_pago = $result['orden_de_pago'];
           $estado = $result['estado'];
           $credor = $result['credor'];
           $empresa = $result['empresa'];
           $cheque = $result['cheque'];
           $ruc = $result['ruc']; */
-
+          if($_SESSION['sessao']==1){
+          for ($total = 0 ; $total < $_SESSION['tamanho'] ; $total++){
          ?>
-
-          <td align="center"><?=$_SESSION['fecha_de_emision']; ?></td>
-          <td align="center"><?=$_SESSION['orden_de_pago']; ?></td>
-          <td align="center"><?=$_SESSION['estado']; ?></td>
-          <td align="center"><?=$_SESSION['credor']; ?></td>
-          <td align="center"><?=$_SESSION['empresa']; ?></td>
-          <td align="center"><?=$_SESSION['cheque']; ?></td>
-          <td align="center"><?=$_SESSION['ruc']; ?></td>
-
-         <?php
-          
-         ?>
+        <tr>
+          <td align="center"><?=$_SESSION['fecha_de_emision'.$total]; ?></td>
+          <td align="center"><?=$_SESSION['orden_de_pago'.$total]; ?></td>
+          <td align="center"><?=$_SESSION['estado'.$total]; ?></td>
+          <td align="center"><?=$_SESSION['credor'.$total]; ?></td>
+          <td align="center"><?=$_SESSION['empresa'.$total]; ?></td>
+          <td align="center"><?=$_SESSION['cheque'.$total]; ?></td>
+          <td align="center"><?=$_SESSION['ruc'.$total]; ?></td>
         </tr>
+         <?php
+        
+          }
+        }
+         ?>
+       
       </tbody>
     </table>
 </body>
@@ -86,6 +112,7 @@ ob_start();
 
 
 <?php
+
 $body = ob_get_clean();
 
         $body = iconv("UTF-8","UTF-8//IGNORE",$body);
--- js/salvaPdf.js
+++ js/salvaPdf.js
@@ -1,31 +1,19 @@
 $("#buttonPdfMobile").click(function(){
 
-	
-
-
-/*	var dadosTabela = [];
-
-	var fecha_de_emision = $("#tabelaMobile tr:even li:eq(0)").html().split(": ")[1];
-	var orden_de_pago = $("#tabelaMobile tr:even li:eq(1)").html().split(": ")[1];
-
-	var login = {
-		email: fecha_de_emision,
-		senha: orden_de_pago
-	};
-
-	var table = JSON.stringify(login);
-	var tablejson = {"dados" : table }; 
-	console.log(tablejson); */
 
 	var dadosTabela = [];
 
-	var fecha_de_emision = $("#tabelaMobile tr:even li:eq(0)").html().split(": ")[1];
-	var orden_de_pago = $("#tabelaMobile tr:even li:eq(1)").html().split(": ")[1];
-		var estado = $("#tabelaMobile tr:even li:eq(2)").html().split(": ")[1];
-		var credor = $("#tabelaMobile tr:even li:eq(3)").html().split(": ")[1];
-		var empresa = $("#tabelaMobile tr:even li:eq(4)").html().split(": ")[1];
-		var cheque = $("#tabelaMobile tr:even li:eq(5)").html().split(": ")[1];
-		var ruc = $("#tabelaMobile tr:even li:eq(6)").html().split(": ")[1];
+	var total = $("#tabelaMobile tr:odd").length;
+
+	console.log("total: "+total);
+	for(let i=1; i<= total; i++){
+		var fecha_de_emision = $("#tabelaMobile tr:eq("+(i*2)+") li:eq(0)").html().split(": ")[1];
+		var orden_de_pago = $("#tabelaMobile tr:eq("+(i*2)+") li:eq(1)").html().split(": ")[1];
+		var estado = $("#tabelaMobile tr:eq("+(i*2)+") li:eq(2)").html().split(": ")[1];
+		var credor = $("#tabelaMobile tr:eq("+(i*2)+") li:eq(3)").html().split(": ")[1];
+		var empresa = $("#tabelaMobile tr:eq("+(i*2)+") li:eq(4)").html().split(": ")[1];
+		var cheque = $("#tabelaMobile tr:eq("+(i*2)+") li:eq(5)").html().split(": ")[1];
+		var ruc = $("#tabelaMobile tr:eq("+(i*2)+") li:eq(6)").html().split(": ")[1];
 		
 
 		console.log(fecha_de_emision);
@@ -34,23 +22,23 @@ $("#buttonPdfMobile").click(function(){
 		console.log(credor);
 		console.log(empresa);
 		console.log(cheque);
-		console.log(ruc);
+		console.log(ruc); 
 
 		//console.log(i);
 
 		var dados = {
-			"fecha_de_emision": fecha_de_emision,
-			"orden_de_pago": orden_de_pago,
-			"estado": estado,
-			"credor": credor,
-			"empresa": empresa,
-			"cheque": cheque,
-			"ruc": ruc,
+			fecha_de_emision: fecha_de_emision,
+			orden_de_pago: orden_de_pago,
+			estado: estado,
+			credor: credor,
+			empresa: empresa,
+			cheque: cheque,
+			ruc: ruc,
 		};
 
-		//dadosTabela.push(dados);
-	//}
-	dadosTabela = JSON.stringify(dados);
+		dadosTabela.push(dados);
+	}
+	dadosTabela = JSON.stringify(dadosTabela);
 
 	console.log(dadosTabela);
 
@@ -90,32 +78,18 @@ $("#buttonPdfMobile").click(function(){
 //*********************************************************************Desktop
 $("#buttonPdfDesktop").click(function(){
 
-	
-
-
-/*	var dadosTabela = [];
-
-	var fecha_de_emision = $("#tabelaMobile tr:even li:eq(0)").html().split(": ")[1];
-	var orden_de_pago = $("#tabelaMobile tr:even li:eq(1)").html().split(": ")[1];
-
-	var login = {
-		email: fecha_de_emision,
-		senha: orden_de_pago
-	};
-
-	var table = JSON.stringify(login);
-	var tablejson = {"dados" : table }; 
-	console.log(tablejson); */
-
 	var dadosTabela = [];
+	var total = $("#tabelaDesktop tbody>tr").length;
 
-		var fecha_de_emision = $("#tabelaDesktop td:eq(0)").html();
-		var orden_de_pago = $("#tabelaDesktop td:eq(1)").html();
-		var estado = $("#tabelaDesktop td:eq(2)").html();
-		var credor = $("#tabelaDesktop td:eq(3)").html();
-		var empresa = $("#tabelaDesktop td:eq(4)").html();
-		var cheque = $("#tabelaDesktop td:eq(5)").html();
-		var ruc = $("#tabelaDesktop td:eq(6)").html();
+	console.log("total: "+total);
+	for(let i=1; i<= total; i++){
+		var fecha_de_emision = $("#tabelaDesktop tr:eq("+i+") td:eq(0)").html();
+		var orden_de_pago = $("#tabelaDesktop tr:eq("+i+") td:eq(1)").html();
+		var estado = $("#tabelaDesktop tr:eq("+i+") td:eq(2)").html();
+		var credor = $("#tabelaDesktop tr:eq("+i+") td:eq(3)").html();
+		var empresa = $("#tabelaDesktop tr:eq("+i+") td:eq(4)").html();
+		var cheque = $("#tabelaDesktop tr:eq("+i+") td:eq(5)").html();
+		var ruc = $("#tabelaDesktop tr:eq("+i+") td:eq(6)").html();
 		
 
 		console.log(fecha_de_emision);
@@ -138,9 +112,9 @@ $("#buttonPdfDesktop").click(function(){
 			"ruc": ruc,
 		};
 
-		//dadosTabela.push(dados);
-	//}
-	dadosTabela = JSON.stringify(dados);
+		dadosTabela.push(dados);
+	}
+	dadosTabela = JSON.stringify(dadosTabela);
 
 	console.log(dadosTabela);
 
@@ -163,7 +137,7 @@ $("#buttonPdfDesktop").click(function(){
 				},
 	        success: function(data) {
 	        	console.log("transmitidos com sucesso!");
-				console.log(data);
+			//	console.log(data);
 				window.location.replace("http://localhost/Digitalizacao/SalvarParaPDF.php");
 			
 	        	}
